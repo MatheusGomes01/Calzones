a06fa6c6ce19ea4c52e616deb6854fe02200342f
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-13 14:13:15 -0300

    trazendo as variaveis a um bd

---
+++ registra_usuario.php
@@ -0,0 +1,21 @@
+<?
+
+	require_once('bd.calzones.php');
+
+	$user = $_POST['usuario'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+
+	$objBd = new db();
+	$link = $objBd->conecta_mysql();
+
+	$sql = " insert into tb_users(usuario, email, senha) values ('$user', '$email', '$senha')";
+
+	//executar a query
+	mysqli_query($link, $sql)){
+		echo 'Uśuário registrado com sucesso';
+	} else{
+		echo 'Erro ao registrar usuário'
+	}
+
+	?>
