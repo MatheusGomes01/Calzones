fccb0a97f61d6cbc80a7bf8aaaaa3a1411018770
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-20 16:54:15 -0300

    validando parte de seguir e achar amigos

--- get_amigos.php
+++ get_amigos.php
@@ -7,23 +7,29 @@ if(!isset($_SESSION['usuario'])){
 
 require_once('bd.calzones.php');
 
+$nome_amigo = $_POST['nome_amigo'];
 $id_usuario = $_SESSION['id_usuario'];
 
 $objBd = new db();
 $link = $objBd->conecta_mysql();
 
-$sql = "select * from tb_users";
+$sql = "select * from tb_users where usuario like '%$nome_amigo%' and id <> $id_usuario";
 
 $resultado_id = mysqli_query($link, $sql);
 
 if ($resultado_id){
 	while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
 		echo '<a href = "#" class = "list-group-item">';
-		echo '<storng>Nome</strong> <small> - email </small>'
+		echo '<storng>'.$registro['usuario'].
+		'</strong> <small>'.$registro['email'].'</small>';
+		echo '<p class="list-group-item-text pull-right">';
+		echo '<button type="button" class="btn btn-info">Seguir</button>';
+		echo '</p>';
+		echo '<div class="clearfix"></div>';
 		echo '</a>';
 	}
 }else{
-	echo 'Amigo inexiste ou erro ao procurar';
+	echo 'Amigo inexistente ou erro ao procurar';
 }
 
 ?>
