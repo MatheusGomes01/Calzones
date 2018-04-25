a4a26be770075de9232e42cb7acb0028d19a9a59
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-25 16:32:03 -0300

    pagina pra add o calzar

--- get_calzando.php
+++ get_calzando.php
@@ -14,7 +14,7 @@ $link = $objBd->conecta_mysql();
 
 $sql = "select date_format(p.dt_Calz, '%d %b %Y %T') as dt_Calz_formatada, p.CalZaquiText,  u.usuario ";
 $sql.= " from tb_calzando as p join tb_users as u on (p.id_user = u.id) ";
-$sql.= " where id_user = $id_usuario order by dt_Calz desc";
+$sql.= " where id_user = $id_usuario or id_user in (select seguidor from seguidores_calz where id_usuario = $id_usuario) order by dt_Calz desc";
 
 $resultado_id = mysqli_query($link, $sql);
 
