5b4794bb84fd46a2be2c9a3e9d905a693f8c2a92
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-25 16:31:47 -0300

    pagina de validando amigos

--- get_amigos.php
+++ get_amigos.php
@@ -13,7 +13,10 @@ $id_usuario = $_SESSION['id_usuario'];
 $objBd = new db();
 $link = $objBd->conecta_mysql();
 
-$sql = "select * from tb_users where usuario like '%$nome_amigo%' and id <> $id_usuario";
+$sql = "select a.*, b.* from tb_users as a
+left join seguidores_calz as b
+on (b.id_usuario = $id_usuario and a.id = b.seguidor)
+where a.usuario like '%$nome_amigo%' and a.id <> $id_usuario";
 
 $resultado_id = mysqli_query($link, $sql);
 
@@ -23,7 +26,24 @@ if ($resultado_id){
 		echo '<storng>'.$registro['usuario'].
 		'</strong> <small>'.$registro['email'].'</small>';
 		echo '<p class="list-group-item-text pull-right">';
-		echo '<button type="button" class="btn btn-info">Seguir</button>';
+
+		$esta_seguindo_sn = isset($registro['seguidor']) && !empty($registro['seguidor']) ? 'S' : 'N';
+
+		$btn_seguir_display = 'block';
+		$btn_deixar_seguir_display = 'block';
+
+		if($esta_seguindo_sn == 'N' ){
+			$btn_deixar_seguir_display = 'none';
+		} else {
+			$btn_seguir_display = 'none';
+		}
+
+		echo '<button type="button"id="btn_seguir_'.$registro['id'].'" class="btn btn-info btn_seguir" style="display: '.$btn_seguir_display.'" data-id_usuario="'.$registro['id'].'">Calzar</button>';
+
+
+		echo '<button type="button" id="btn_deixar_seguir_'.$registro['id'].'" style="display: '.$btn_deixar_seguir_display.'" class="btn btn-danger btn_deixar_seguir" data-id_usuario="'.$registro['id'].'">Deixar em paz</button>';
+
+
 		echo '</p>';
 		echo '<div class="clearfix"></div>';
 		echo '</a>';
